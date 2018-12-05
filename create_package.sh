1036775b5fd83057a5160deb2879982f23b1d49c
Dawid Rycerz <13086223+knightdave@users.noreply.github.com>
2018-12-05 22:13:14 +0100

    Initial commit

---
+++ create_package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+[[ ! -d ./automatron ]] && mkdir automatron
+[[ ! -d ./automatron/packages ]] && mkdir automatron
+wget -O ./automatron/Miniconda.sh https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh
+pip download -d ./automatron/packages -r requirements.txt
+
+cat << EOF >> ./automatron/install.sh
+#!/bin/bash
+
+bash Miniconda2-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+pip install --no-index --find-links="./packages/" ansible
+EOF
+
+[[ ! -d ./makeself ]] && git clone https://github.com/megastep/makeself.git
+./makeself/makeself.sh automatron automatron.sh "Conda distribution with automation tools" ./install.sh
+rm -rf ./automatron
